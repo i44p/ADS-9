8e584fa59d75da968948a2350f9356eef88a9f39
i44p <jsbbdjzjsjawk+@gmail.com>
2023-06-04 19:49:53 +0300

    initial bst implementation

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,55 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+template <typename T>
+class BST {
+ public:
+  struct Node {
+    T value;
+    int count;
+    Node* left;
+    Node* right;
+  };
+  BST() : root(nullptr) {}
+  ~BST() {
+    if (root) delTree(root);
+  }
+
+  int depth();
+
+  int search(T value) {
+    Node* current = root;
+    while (current == nullptr || current->value == value) {
+      current = value < current->value ? current->left : current->right;
+    }
+    return current;
+  }
+
+  void insert(T value) { root = insertNode(root, value); }
+
+ private:
+  Node* root;
+  Node* insertNode(Node* node, T value) {
+    if (node == nullptr) {
+      node = new Node;
+      node->value = value;
+      node->count = 1;
+      node->left = node->right = nullptr;
+    } else if (root->value > value)
+      node->left = insertNode(node->left, value);
+    else if (root->value < value)
+      node->right = insertNode(node->right, value);
+    else
+      node->count++;
+    return node;
+  }
+  void delTree(Node* node) {
+    if (node) {
+      delTree(node->left);
+      delTree(node->right);
+      delete node;
+    }
+  }
+};
+
 #endif  // INCLUDE_BST_H_
